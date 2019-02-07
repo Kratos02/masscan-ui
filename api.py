e94a8db3492c828b3707099fa3977f559310cef3
David Tavarez <davidftavarez@gmail.com>
2019-02-06 22:08:13 -0400

    details base:

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 from sanic import Sanic
-from sanic.response import json as s_jon
+from sanic.response import json as s_json
 import configparser
 from os import listdir, path, stat
 from os.path import isfile, join
@@ -10,7 +10,7 @@ from os.path import isfile, join
 app = Sanic()
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 async def home(request):
     current_path = path.dirname(path.abspath(__file__))
     results_path = 'results'
@@ -38,7 +38,19 @@ async def home(request):
             }
         )
 
-    return s_jon({'summary': {'files': len(files)}, 'details': details})
+    return s_json({'summary': {'files': len(files)}, 'details': details})
+
+
+@app.route('/details/<file:string>', methods=['GET'])
+async def integer_handler(request, file):
+    current_path = path.dirname(path.abspath(__file__))
+    results_path = 'results'
+    absolute_path = path.join(current_path, results_path)
+    file_path = path.join(absolute_path, file)
+    with open(file_path) as f:
+        data = json.load(f)
+
+    return s_json({'file': data[0]})
 
 
 if __name__ == '__main__':
