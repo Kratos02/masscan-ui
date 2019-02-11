051c9fe9c5e07b77e5577caea4b8d09a6144da0a
David Tavarez <davidftavarez@gmail.com>
2019-02-11 16:04:42 -0400

    getting results details

--- api.py
+++ api.py
@@ -7,21 +7,25 @@ from os.path import isfile, join, exists
 from sanic import Sanic
 from sanic.response import json as s_json
 
-from helpers.masscan import get_ip_list
+from helpers.masscan import get_ip_list, get_results_by_ip
 
 app = Sanic()
 
 
-@app.route('/', methods=['GET'])
-async def home(request):
+def get_results_path():
     current_path = path.dirname(path.abspath(__file__))
     results_path = 'results'
     absolute_path = path.join(current_path, results_path)
+    return absolute_path
+
+@app.route('/', methods=['GET'])
+async def home(request):
+    absolute_path = get_results_path()
 
     if not exists(absolute_path):
          return s_json({'summary': {}, 'details': {}})
 
-    files = [f for f in listdir(path=absolute_path) if isfile(join(results_path, f))]
+    files = [f for f in listdir(path=absolute_path) if isfile(join(absolute_path, f))]
 
     details = []
     for result in files:
@@ -29,34 +33,40 @@ async def home(request):
         info = stat(file_path)
         with open(file_path) as f:
             data = json.load(f)
-        targets = []
-        for line in data:
-            ip = line.get('ip', None)
-            if ip not in targets:
-                targets.append(ip)
+            targets = get_ip_list(data)
 
-        details.append(
-            {
-                'filename': result,
-                'timestamp': datetime.datetime.fromtimestamp(info.st_birthtime),
-                'targets': len(targets),
-                '_link': '/details/{}'.format(result)
-            }
-        )
+            details.append(
+                {
+                    'filename': result,
+                    'timestamp': datetime.datetime.fromtimestamp(info.st_birthtime),
+                    'targets': len(targets),
+                    '_link': '/details/{}'.format(result)
+                }
+            )
 
     return s_json({'summary': {'files': len(files)}, 'details': details})
 
+@app.route('/details/<file:string>/<ip:string>', methods=['GET'])
+async def ip_details_handler(request, file, ip):
+    file_path = path.join(get_results_path(), file)
+
+    with open(file_path) as f:
+        data = json.load(f)
+
+    return s_json(get_results_by_ip(data, ip))
 
 @app.route('/details/<file:string>', methods=['GET'])
-async def integer_handler(request, file):
-    current_path = path.dirname(path.abspath(__file__))
-    results_path = 'results'
-    absolute_path = path.join(current_path, results_path)
-    file_path = path.join(absolute_path, file)
+async def file_details_handler(request, file):
+    file_path = path.join(get_results_path(), file)
 
-    list_of_ip = get_ip_list(file_path)
+    with open(file_path) as f:
+        data = json.load(f)
+    results = {}
+    targets = get_ip_list(data)
+    for ip in targets:
+        results[ip] = get_results_by_ip(data, ip)
+    return s_json(results)
 
-    return s_json(list_of_ip)
 
 
 if __name__ == '__main__':
--- helpers/masscan.py
+++ helpers/masscan.py
@@ -3,14 +3,46 @@ from socket import inet_aton
 import struct
 
 
-def get_ip_list(file_path):
+def get_ip_list(data):
     list_of_ips = []
-    with open(file_path) as f:
-        data = json.load(f)
-        for item in data:
-            ip = item.get("ip")
-            if ip not in list_of_ips:
-                list_of_ips.append(ip)
+
+    for item in data:
+        ip = item.get("ip")
+        if ip not in list_of_ips:
+            list_of_ips.append(ip)
 
     return sorted(list_of_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
 
+
+def get_results_by_ip(data, ip):
+    results = []
+
+    ports = []
+    for item in data:
+        if item.get("ip") == ip:
+            ports.append(item.get("ports"))
+    for port in ports:
+        item = port[0]
+        record = {
+            "port": item.get("port"),
+            "protocol": item.get("proto", None),
+            "status": item.get("status", None),
+            "ttl": item.get("ttl", None),
+            "service": [],
+        }
+        if not is_record_in_results(record, results):
+            results.append(record)
+        banner = item.get("service", None)
+        if banner:
+            for _result in results:
+                if _result.get("port") == record.get("port"):
+                    _result.get("service").append({"content": banner.get("banner"), "name": banner.get("name")})
+
+    return results
+
+
+def is_record_in_results(record, results):
+    for item in results:
+        if item.get("port") == record.get("port"):
+            return True
+    return False
