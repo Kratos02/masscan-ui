0f50396b567c93333b600765a193d12808ad9515
David Tavarez <davidftavarez@gmail.com>
2019-02-13 22:23:04 -0400

    serving static file

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ import json
 from os import listdir, path, stat
 from os.path import isfile, join, exists
 
-from sanic.response import json as s_json
+from sanic.response import file as sanic_file, json as sanic_json
 
 from api import app
 from api.logic import get_ip_list, get_results_by_ip, get_results_path
@@ -11,12 +11,16 @@ from api.logic import get_ip_list, get_results_by_ip, get_results_path
 
 @app.route('/', methods=['GET'])
 async def home(request):
+    return await sanic_file('./assets/dashboard.html')
+
+@app.route('/summary', methods=['GET'], version=1)
+async def summary(request):
     summary = {}
 
     absolute_path = get_results_path()
 
     if not exists(absolute_path):
-        return s_json({'summary': summary, 'details': {}})
+        return sanic_json({'summary': summary, 'details': {}})
 
     files = [f for f in listdir(path=absolute_path) if isfile(join(absolute_path, f))]
 
@@ -48,33 +52,33 @@ async def home(request):
                     'filename': result,
                     'timestamp': datetime.datetime.fromtimestamp(info.st_birthtime),
                     'targets': len(targets),
-                    '_link': '/details/{}'.format(result),
+                    '_link': '/v1/details/{}'.format(result),
                     'open': _open_ports, 'closed': _closed_ports
                 }
             )
 
-    return s_json({'summary': {'scans': len(files), "open": open_ports, "closed": closed_ports}, 'details': details})
+    return sanic_json({'summary': {'scans': len(files), "open": open_ports, "closed": closed_ports}, 'details': details})
 
 
-@app.route('/details/<file:string>/<ip:string>', methods=['GET'])
+@app.route('/details/<file:string>/<ip:string>', methods=['GET'],  version=1)
 async def ip_details_handler(request, file, ip):
     file_path = path.join(get_results_path(), file)
 
     if not exists(file_path):
-        return s_json({})
+        return sanic_json({})
 
     with open(file_path) as f:
         data = json.load(f)
 
-    return s_json(get_results_by_ip(data, ip))
+    return sanic_json(get_results_by_ip(data, ip))
 
 
-@app.route('/details/<file:string>', methods=['GET'])
+@app.route('/details/<file:string>', methods=['GET'],  version=1)
 async def file_details_handler(request, file):
     file_path = path.join(get_results_path(), file)
 
     if not exists(file_path):
-        return s_json({})
+        return sanic_json({})
 
     with open(file_path) as f:
         data = json.load(f)
@@ -82,4 +86,4 @@ async def file_details_handler(request, file):
     targets = get_ip_list(data)
     for ip in targets:
         results[ip] = get_results_by_ip(data, ip)
-    return s_json(results)
+    return sanic_json(results)
